3b34c51d7546f8002b2231c6340a1259d8f65ecf
Lars Kanis <lars@greiz-reinsdorf.de>
2024-12-23 14:22:02 +0100

    Add script for local build and upload into the pacman repository
    This is needed for ARM64, since the github CI doesn't provide ARM64 build environments.

---
+++ local-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+export DEPLOY_REPO_NAME=oneclick/rubyinstaller2-packages
+export DEPLOY_TAG=ci.ri2
+
+source ./ci-library.sh
+
+mkdir -p artifacts
+cd mingw-w64-ruby-head
+rm -f snapshot-master.tar.xz
+makepkg-mingw --noconfirm --nocheck --syncdeps --rmdeps --cleanbuild --sign -f
+cp *.pkg.tar.zst *.pkg.tar.zst.sig ../artifacts/
+cd ../artifacts/
+rm -f ci.ri2.*
+create_pacman_repository 'ci.ri2'
+cd ..
+rake upload -- artifacts/*.pkg.tar.zst artifacts/*.pkg.tar.zst.sig artifacts/*.db.tar.zst artifacts/*.db.tar.zst.sig artifacts/*.db artifacts/*.db.sig artifacts/*.files.tar.zst artifacts/*.files.tar.zst.sig artifacts/*.files artifacts/*.files.sig
